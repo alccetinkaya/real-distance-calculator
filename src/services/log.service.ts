fba4ab6b25db72163209cad1026043f01c36619a
Ali Cuneyt CETINKAYA <alicuneytcetinkaya@gmail.com>
2021-12-12 22:25:22 +0300

    File log service has added

--- src/services/log.service.ts
+++ src/services/log.service.ts
@@ -1,18 +1,54 @@
 import { DistanceData } from "../models/data.model"
+import { getDate_YYYYMMDD, getDate_YYYYMMDD_HHMMSS } from "../utilities";
+import fs from 'fs';
 
 export interface LogServiceInterface {
     errorLog(error: string): void
-    printDistanceData(data: DistanceData[]): void
+    infoLog(info: string): void
 }
 
 export class ConsoleLogService implements LogServiceInterface {
     errorLog(error: string): void {
-        console.log(error)
+        console.log("\x1b[31mERROR: \x1b[0m", error);
     }
 
-    printDistanceData(data: DistanceData[]): void {
-        for (const distanceData of data) {
-            console.log(`${distanceData.originPoint.name.padEnd(25)} -> ${distanceData.destinationPoint.name.padEnd(25)} || Distance: ${distanceData.distance.padEnd(15)} Duration: ${distanceData.duration}`);
-        }
+    infoLog(info: string): void {
+        console.log("\x1b[33mINFO: \x1b[0m", info);
+    }
+}
+
+export class FileLogService implements LogServiceInterface {
+    static _instance: FileLogService;
+    _dir: string;
+    _name: string;
+    _file: any;
+
+    private constructor() {
+        this._dir = "./";
+        this._name = getDate_YYYYMMDD(Date.now()) + ".log";
+        this.createFileSync();
+    }
+
+    static get Instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    errorLog(error: string): void {
+        this.write(`[${getDate_YYYYMMDD_HHMMSS(Date.now())}][ERROR] ${error}`);
+    }
+
+    infoLog(info: string): void {
+        this.write(`[${getDate_YYYYMMDD_HHMMSS(Date.now())}][INFO] ${info}`);
+    }
+
+    async createFileSync() {
+        fs.mkdirSync(this._dir, { recursive: true });
+        this._file = fs.createWriteStream(this._dir + this._name, {
+            flags: 'a'
+        });
+    }
+
+    write(data: string) {
+        this._file.write(data + "\n");
     }
 }
