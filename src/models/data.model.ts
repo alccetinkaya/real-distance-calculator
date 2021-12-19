524cb7f6c5f6aebbeee56415398b7e6b7bea4647
Ali Cuneyt CETINKAYA <alicuneytcetinkaya@gmail.com>
2021-12-19 14:06:39 +0300

    User input daha has added

--- src/models/data.model.ts
+++ src/models/data.model.ts
@@ -1,3 +1,4 @@
+import { UserInputNames } from "../services/user-input.service";
 import { Point } from "./point.model";
 
 export interface ConfigData {
@@ -25,3 +26,15 @@ export interface PrintData {
     duration: DurationData;
 }
 
+export interface UserInputData {
+    originFileName: string;
+    originPointSelect: UserInputNames;
+    originPointLen: number;
+    destinationFileName: string;
+    destinationPointSelect: UserInputNames;
+    destinationPointLen: number;
+    departureTime_DD_MM_YYYY: string;
+    departureTime_HH_MM: string;
+    departureEpochTime: number;
+    outputFileName: string;
+}
