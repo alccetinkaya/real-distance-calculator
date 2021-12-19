4727341d6f4a884e8433ba1dbd65e95bc89a96fb
Ali Cuneyt CETINKAYA <alicuneytcetinkaya@gmail.com>
2021-12-19 14:07:26 +0300

    Date and time methods have added

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,4 +1,5 @@
 const prompts = require('prompts');
+import moment from 'moment';
 
 export function exit() {
     console.log('\nPress any key to exit..');
@@ -15,14 +16,6 @@ export async function getPromptResult<T>({ type = null, name = "default", messag
     return response[name];
 }
 
-export function getDate_YYYYMMDD(timestamp: number): string {
-    return new Date(timestamp).toJSON().slice(0, 10);
-}
-
-export function getDate_YYYYMMDD_HHMMSS(timestamp: number): string {
-    return new Date(timestamp).toISOString().replace('T', ' ').substring(0, 19);
-}
-
 export function getMaxLenOfListElement(list: any[]) {
     let size: number = 0;
     list.forEach(element => {
@@ -35,4 +28,40 @@ export function getMaxLenOfListElement(list: any[]) {
         size = size < str.length ? str.length : size;
     });
     return size;
+}
+
+export function getDateAsStr(timestampAsMs:number, format: string): string {
+    return moment(timestampAsMs).format(format);
+}
+
+export function getDateAsNum(timestampAsMs:number, format: string): number {
+    return Number(moment(timestampAsMs).format(format));
+}
+
+export function getCurrentDateAsStr(format: string): string {
+    return moment().format(format);
+}
+
+export function getCurrentDateAsNum(format: string): number {
+    return Number(moment().format(format));
+}
+
+export function getEpochTimeAsMs(date: string, format: string): number {
+    return moment(date, format).valueOf();
+}
+
+export function getEpochTimeAsSec(date: string, format: string): number {
+    return Math.round(getEpochTimeAsMs(date, format) / 1000);
+}
+
+export function getCurrentEpochTimeAsMs(): number {
+    return moment().valueOf();
+}
+
+export function getCurrentEpochTimeAsSec(): number {
+    return Math.round(getCurrentEpochTimeAsMs() / 1000);
+}
+
+export function getUtcOffset(): string {
+    return new Date().toTimeString().split(" ")[1].slice(0,6);
 }
