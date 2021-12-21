34b8d258996b5527fde7bc93766299290072c5c6
Ali Cuneyt CETINKAYA <alicuneytcetinkaya@gmail.com>
2021-12-21 22:01:13 +0300

    Excel service has fixed

--- src/services/excel.service.ts
+++ src/services/excel.service.ts
@@ -1,29 +1,56 @@
-const Excel = require('read-excel-file/node');
-//const Excel = require('exceljs')
+const Excel = require('exceljs')
 
 export class ExcelService {
-    readExcelFile(filePath: string) {
-        Excel(filePath).then((rows: any) => {
-            return rows;
-        });
+    _workbook: any;
+
+    constructor() {
+        this._workbook = this.createWorkbook();
     }
 
-    readExcelFileSync(filePath: string): Promise<any[]> {
-        return new Promise((resolve, _reject) => {
-            Excel(filePath).then((rows: any[]) => {
-                resolve(rows);
-            });
-        });
+    createWorkbook() {
+        return new Excel.Workbook();
     }
 
-    async readPointsFromExcelFileSync(filePath: string): Promise<any[]> {
+    addWorksheet(sheetName: string) {
+        this._workbook.addWorksheet(sheetName);
+    }
+
+    getWorksheet(sheetName: string) {
+        return this._workbook.getWorksheet(sheetName);
+    }
+
+    addRow(sheetName: string, data: any) {
+        this.getWorksheet(sheetName).addRow(data);
+    }
+
+    getRow(sheetName: string, index: number) {
+        return this.getWorksheet(sheetName).getRow(index);
+    }
+
+    async readFile(fileName: string, sheetName: string = "Sheet1", discardHeader: boolean = false): Promise<any[]> {
         return new Promise((resolve, reject) => {
-            Excel(filePath)
-                .then((rows: any[]) => {
-                    rows.shift(); // discard the first line
+            this._workbook.xlsx.readFile(fileName)
+                .then(() => {
+                    let worksheet = this._workbook.getWorksheet(sheetName);
+                    if (!worksheet) reject(`Worksheet ${sheetName} could not found`);
+
+                    let rows: any[] = [];
+                    worksheet.eachRow({}, (row: any, rowNumber: number) => {
+                        if (discardHeader === true && rowNumber == 1) return;
+
+                        let tempList: any[] = [];
+                        row.values.forEach((element: any) => {
+                            tempList.push(element);
+                        });
+                        rows.push(tempList);
+                    });
                     resolve(rows);
                 })
                 .catch((error: any) => reject(error));
         });
+    }
+
+    async writeFile(fileName: string) {
+        await this._workbook.xlsx.writeFile(fileName);
     }
 }
